5d33a6dbf2320d0d5823ffb6607688da457b3d0b
Mamoru Kijima <sadako0215@gmail.com>
2016-05-12 08:30:35 +0900

    stylus通るようにした

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,14 +1,41 @@
 'use strict';
 
 import gulp from 'gulp';
+
+// default file system node
+import fs from 'fs'
+
+// compile for stylus
+import stylus from 'gulp-stylus';
+
+import notify from 'gulp-notify';
+
+// module, and get stream
 import browserify from "browserify";
 import source from "vinyl-source-stream";
 
-gulp.task("default", () => {
-  browserify({
-      entries: ["./src/app.es6"]
-    })
-    .bundle()
-    .pipe(source("app.js"))
-    .pipe(gulp.dest("./build"));
+// watche for error
+import plumber from "gulp-plumber";
+
+// broserify, plumber don't stop.
+import handleErrors from "./handleErrors.js";
+
+// live reload
+import browser from "browser-sync";
+
+// gulp.task("default", () => {
+//   browserify({
+//       entries: ["./app/app.es6"]
+//     })
+//     .bundle()
+//     .pipe(source("app.js"))
+//     .pipe(gulp.dest("./build"));
+// });
+
+gulp.task('stylus', () => {
+  gulp.src('app/stylus/**/*.stylus')
+    .pipe(plumber())
+    .pipe(stylus())
+    .pipe(gulp.dest('./dist/stylesheet'))
+    .pipe(browser.reload({stream:true}));
 });
