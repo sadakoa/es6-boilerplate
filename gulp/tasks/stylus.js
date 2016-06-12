7183006492646466ad30d73c252c9957fa503364
mamoru_kijima <mamoru.kijima@dena.jp>
2016-06-12 13:36:55 +0900

    autoprefixer追加

--- gulp/tasks/stylus.js
+++ gulp/tasks/stylus.js
@@ -1,15 +1,17 @@
 import gulp from 'gulp';
 import notify from 'gulp-notify';
-import plumber from "gulp-plumber";
+import plumber from 'gulp-plumber';
+import autoprefixer from 'gulp-autoprefixer';
 import stylus from 'gulp-stylus';
 import raname from 'gulp-rename';
-import browser from "browser-sync";
-let conf = require('../config.js');
+import browser from 'browser-sync';
+const conf = require('../config.js');
 
 gulp.task('stylus', () => {
  return gulp.src('app/stylus/app.styl')
 .pipe(plumber({errorHandler: notify.onError('<%= error.message %>')}))
 .pipe(stylus())
+.pipe(autoprefixer())
 .pipe(raname('app.css'))
 .pipe(gulp.dest(conf.stylus.dest))
 .pipe(browser.reload({stream:true}));
